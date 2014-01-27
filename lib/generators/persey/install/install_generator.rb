557e8c323134c7a8ab722197ad54f45dad4b9aeb
Andrey Kumanyaev <me@zzet.org>
2014-01-28 01:19:32 +0400

    Update rails generator

--- lib/generators/persey/install/install_generator.rb
+++ lib/generators/persey/install/install_generator.rb
@@ -5,8 +5,30 @@ module Persey
 
       def create_configuration
         copy_file("config.rb", "config/config.rb")
+        inject_into_file 'config/application.rb', before: "module #{Rails.application.class.parent_name}" do
+          <<-'RUBY'
+require "persey"
+require File.expand_path('../config', __FILE__)
+
+          RUBY
+        end
       end
 
+      def ignore_configuration
+        if File.exists?("config/config.rb")
+          append_to_file("config/config.rb") do
+            <<-EOF.strip_heredoc
+
+              # Define method in #{Rails.application.class.parent_name}
+              module #{Rails.application.class.parent_name}
+                def self.config
+                  Persey.config
+                end
+              end
+              EOF
+          end
+        end
+      end
     end
   end
 end
