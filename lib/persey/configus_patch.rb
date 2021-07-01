b7f80b99fe2d6528f3f5a8df5a8aefa6930aa7ff
Ivan Nemytchenko <nemytchenko@gmail.com>
2021-07-01 20:56:39 +0200

    Make it possible to get hash of options

--- lib/persey/configus_patch.rb
+++ lib/persey/configus_patch.rb
@@ -2,6 +2,8 @@ class Configus::Config
   def method_missing(meth, *args, &blk)
     if Persey.config.to_hash.has_key?(meth)
       Persey.config.send(meth)
+    elsif meth == :to_hash
+      Persey.config.to_hash
     else
       raise "'#{meth}' key does not exist in your config"
     end
